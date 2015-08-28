3f87bf0febe6449f505cc170927e0bd6f0e3d755
alexandra <student@epicodus.com>
2015-08-28 09:53:36 -0700

    write failing test for save and getAll in Store class

--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -81,6 +81,44 @@
             $this->assertEquals($id, $result);
         }
 
+        function test_save()
+        {
+            //Arrange
+            $name = "The Shoe Store";
+            $location = "432 SW Tootsies Ave";
+            $phone = "503-555-5555";
+            $test_store = new Store($name, $location, $phone);
+
+            //Act
+            $test_store->save();
+
+            //Assert
+            $result = Store::getAll();
+            $this->assertEquals($test_store, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $name = "The Shoe Store";
+            $location = "432 SW Tootsies Ave";
+            $phone = "503-555-5555";
+            $test_store = new Store($name, $location, $phone);
+            $test_store->save();
+
+            $name2 = "Boots n Stuff";
+            $location2 = "900 WalkAlot Blvd.";
+            $phone2 = "971-202-0292";
+            $test_store2 = new Store($name, $location, $phone);
+            $test_store2->save();
+
+            //Act
+            $result = Store::getAll();
+
+            //Assert
+            $this->assertEquals([$test_store, $test_store2], $result);
+        }
+
 
 
     }
