0f7f2c72ecc1faf04b6e8223ce2fc1f323a7488e
alexandra <student@epicodus.com>
2015-08-28 11:11:11 -0700

    write failing tests for addStore and getStores in Brand class

--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -84,5 +84,54 @@
             $result = Brand::getAll();
             $this->assertEquals([], $result);
         }
+
+        function test_addStore()
+        {
+            //Arrange
+            $name = "Crocs";
+            $test_brand = new Brand($name);
+            $test_brand->save();
+
+            $name = "The Shoe Store";
+            $location = "432 SW Tootsies Ave";
+            $phone = "503-555-5555";
+            $test_store = new Store($name, $location, $phone);
+            $test_store->save();
+
+            //Act
+            $test_brand->addStore($test_store);
+
+            //Assert
+            $result = $test_brand->getStores();
+            $this->assertEquals([$test_store], $result);
+        }
+
+        function test_getStores()
+        {
+            //Arrange
+            $name = "Crocs";
+            $test_brand = new Brand($name);
+            $test_brand->save();
+
+            $name = "The Shoe Store";
+            $location = "432 SW Tootsies Ave";
+            $phone = "503-555-5555";
+            $test_store = new Store($name, $location, $phone);
+            $test_store->save();
+
+            $name2 = "Boots n Stuff";
+            $location2 = "900 WalkAlot Blvd.";
+            $phone2 = "971-202-0292";
+            $test_store2 = new Store($name, $location, $phone);
+            $test_store2->save();
+
+            //Act
+            $test_brand->addStore($test_store);
+            $test_brand->addStore($test_store2);
+
+            //Assert
+            $result = $test_brand->getStores();
+            $this->assertEquals([$test_store, $test_store2], $result);
+        }
     }
  ?>
