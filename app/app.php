c5f13530a584dfc2424736287fe3f6c887a9734c
alexandra <student@epicodus.com>
2015-08-28 11:25:29 -0700

    create app.php with homepage routing.

---
+++ app/app.php
@@ -0,0 +1,26 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Brand.php";
+    require_once __DIR__.'/../src/Store.php';
+
+    $app = new Silex\Application();
+
+    $server = 'mysql:host=localhost;dbname=shoes';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__."/../views"
+    ));
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    //homepage with options to view stores and brands
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
+
+
+ ?>
