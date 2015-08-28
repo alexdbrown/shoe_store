2766ed57e2600e82d427f096f67027282190135e
alexandra <student@epicodus.com>
2015-08-28 11:07:47 -0700

    write methods to pass getBrand and addBrands tests for Store class

--- src/Store.php
+++ src/Store.php
@@ -72,12 +72,28 @@
         //class interaction methods
         function addBrand($brand)
         {
-
+            $GLOBALS['DB']->exec("INSERT INTO stores_brands (store_id, brand_id) VALUES (
+                {$this->getId()}, {$brand->getId()}
+            );");
         }
 
         function getBrands()
         {
+            $brands_query = $GLOBALS['DB']->query(
+                "SELECT brands.* FROM
+                    stores JOIN stores_brands ON (stores.id = stores_brands.store_id)
+                           JOIN brands        ON (stores_brands.brand_id = brands.id)
+                WHERE stores.id = {$this->getId()};"
+            );
 
+            $matching_brands = array();
+            foreach ($brands_query as $brand) {
+                $name = $brand['name'];
+                $id = $brand['id'];
+                $new_brand = new Brand($name, $id);
+                array_push($matching_brands, $new_brand);
+            }
+            return $matching_brands;
         }
 
         //static methods
