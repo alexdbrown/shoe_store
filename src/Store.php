334dfcfabd7e77df318b752ba3b547d7817d6a56
alexandra <student@epicodus.com>
2015-08-28 09:35:06 -0700

    Add Store class outline to Store.php

---
+++ src/Store.php
@@ -0,0 +1,30 @@
+<?php
+    class Store
+    {
+        private $name;
+        private $location;
+        private $phone;
+        private $id:
+
+        function __construct($name, $location, $phone, $id = null)
+        {
+            $this->name = $name;
+            $this->location = $location;
+            $this->phone = $phone;
+            $this->id = $id;
+        }
+
+        //getters and setters
+        function getName()
+        {
+            
+        }
+
+        //database methods
+
+        //class interaction methods
+
+        //static methods
+    }
+
+ ?>
