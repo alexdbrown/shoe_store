c7ae4bd0d76ca0ec3a2727cbd537a65d46e2b0e5
alexandra <student@epicodus.com>
2015-08-28 10:30:25 -0700

    add backbone for Brand class in Brand.php

---
+++ src/Brand.php
@@ -0,0 +1,56 @@
+<?php
+    class Brand
+    {
+        private $name;
+        private $id;
+
+        function __construct($name, $id = null)
+    {
+        $this->name = $name;
+        $this->id = $id;
+    }
+
+    //getters and setters
+    function getName()
+    {
+        return $this->name;
+    }
+
+    function setName($new_name)
+    {
+        $this->name = (string) $new_name;
+    }
+
+    function getId()
+    {
+        return $this->id;
+    }
+
+    //database methods
+    function save()
+    {
+
+    }
+
+    //class interaction methods
+    function addStore($store)
+    {
+
+    }
+
+    function getStores()
+    {
+
+    }
+
+    //static methods
+    static function getAll()
+    {
+
+    }
+
+    static function deleteAll()
+    {
+
+    }
+ ?>
