40478bab5c1a5698fbdd3178c3eeef71724ffa7f
alexandra <student@epicodus.com>
2015-08-28 09:43:38 -0700

    write method that passes getName test in Store class

--- src/Store.php
+++ src/Store.php
@@ -17,14 +17,80 @@
         //getters and setters
         function getName()
         {
+            return $this->name;
+        }
+
+        function setName($new_name)
+        {
 
         }
 
+        function getLocation()
+        {
+
+        }
+
+        function setLocation($new_location)
+        {
+
+        }
+
+        function getPhone()
+        {
+
+        }
+
+        function setPhone($new_phone)
+        {
+
+        }
+
+        function getId()
+        {
+
+        }
         //database methods
+        function save()
+        {
+
+        }
+
+        function update()
+        {
+
+        }
+
+        function delete()
+        {
+
+        }
 
         //class interaction methods
+        function addBrand($brand)
+        {
+
+        }
+
+        function getBrands()
+        {
+
+        }
 
         //static methods
+        static function getAll()
+        {
+
+        }
+
+        static function deleteAll()
+        {
+
+        }
+
+        static function find(){
+
+        }
+
     }
 
  ?>
