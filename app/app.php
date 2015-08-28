490357bb25c28fd44b61711ecd721fc28620f229
alexandra <student@epicodus.com>
2015-08-28 15:51:54 -0700

    add zipped mysql files to project folder

--- app/app.php
+++ app/app.php
@@ -19,31 +19,31 @@
 
     //homepage with options to view stores and brands
     $app->get("/", function() use ($app) {
-        return $app['twig']->render('index.html.twig');
+        return $app['twig']->render('index.html.twig'array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
     });
 
     //Store landing page displaying all stores, incluing a form to add a new store
     $app->get("/stores", function() use ($app) {
-        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
+        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
     });
 
     //adds a store to the list of stores on the same page
     $app->post("/add_store", function() use ($app) {
         $store = new Store($_POST['name'], $_POST['location'], $_POST['phone']);
         $store->save();
-        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
+        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
     });
 
     //deletes all stores and returns to stores page
     $app->post("/delete_all_stores", function() use ($app) {
         Store::deleteAll();
-        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
+        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
     });
 
     //goes to an update page for a store
     $app->get("/stores/{id}/edit", function($id) use ($app) {
         $store = Store::find($id);
-        return $app['twig']->render('store_edit.html.twig', array('stores' => $store));
+        return $app['twig']->render('store_edit.html.twig', array('store' => $store, 'stores' => Store::getAll(), 'brands' => Brand::getAll()));
     });
 
 
@@ -51,26 +51,26 @@
     $app->patch("/stores/{id}", function($id) use ($app) {
         $store = Store::find($id);
         $store->update($_POST['name'], $_POST['location'], $_POST['phone']);
-        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
+        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
     });
 
     //deletes an individual store
     $app->delete("/stores/{id}/delete", function($id) use ($app) {
         $store = Store::find($id);
         $store->delete();
-        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
+        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
     });
 
     //lists all brands and allows user to make a new brand
     $app->get("/brands", function() use ($app) {
-        return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll()));
+        return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll(), 'brands' => Brand::getAll()));
     });
 
     //adds brand to the list of brands on the page
     $app->post("/add_brand", function() use ($app) {
         $brand = new Brand($_POST['name']);
         $brand->save();
-        return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll()));
+        return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll(), 'brands' => Brand::getAll()));
     });
 
     //deletes all brands
@@ -83,7 +83,6 @@
     //     $
     //
     // })
-
 
 
     return $app;
